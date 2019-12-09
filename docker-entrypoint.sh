84866718f32dd7d6c469be67f818a8d82be20093
root <root@ctf.lullabot.com>
2019-12-09 11:59:25 -0500

    trust local proxy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -41,6 +41,7 @@ python manage.py db upgrade
 echo "Starting CTFd"
 exec gunicorn 'CTFd:create_app()' \
     --bind '0.0.0.0:8000' \
+    --forwarded-allow-ips="*" \
     --workers $WORKERS \
     --worker-tmp-dir "$WORKER_TEMP_DIR" \
     --worker-class "$WORKER_CLASS" \
